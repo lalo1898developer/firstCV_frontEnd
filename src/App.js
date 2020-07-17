9bbc96514273ae08c6d7e61d27bdb8b9fd2e35e6
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-16 22:23:52 -0500

    Agrega archivos base para app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Fragment } from 'react';
-import NewForm from './components/NewForm/';
+
+// Components
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+
+// Contexts
+import AuthContextProvider from './contexts/AuthContext';
+
+// Misc
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container } from 'reactstrap';
+import Routes from './Routes';
 
 function App() {
   return (
     <Fragment>
-      <NewForm/>
+      <Router>
+        <AuthContextProvider>
+            <Navigation />
+            <Container className="mt-4">
+              <Switch>
+                { Routes }
+              </Switch>
+            </Container>
+            <Footer />
+        </AuthContextProvider>
+      </Router>
     </Fragment>
   );
 }
