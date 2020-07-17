5d84b261f63a022d1c132e0667ea91c5321dbe1c
Luis Aguilar - Citrust <hola@citrusthn.com>
2020-07-17 15:45:24 -0600

    Update NewForm

--- src/components/ViewCV/index.js
+++ src/components/ViewCV/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Row, Col, Input, Button, Container, Image } from 'reactstrap';
+import './viewCV.css'
 
 const ViewCV = () => {
+
+
   return (
     <React.Fragment>
-      
+      <Container>
+        <Row className="py-3" className="header" >
+          <h2 className="title col-12 text-center">Nombre Completo</h2>
+        <div className="coverPhoto">
+          <img src="http://shorturl.at/zACNS" />
+        </div>
+        </Row>
+        <Row className="academica mt-6">
+          <h2>Formación academica</h2>
+          <table>
+            <ul>
+              <li>Nombre de Escuela</li>
+              <li>Año de ingreso</li>
+              <li>Año de Egreso</li>
+              <li>Carrera</li>
+            </ul>
+          </table>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
